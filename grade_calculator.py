c171df9e0e7fa72d09b9974a54c95ef4ce2b4f96
seh4835 <sehersanghani5@gmail.com>
2026-01-21 07:42:22 +0530

    update

--- grade_calculator.py
+++ grade_calculator.py
@@ -12,3 +12,28 @@ def get_grade_and_message(marks):
     else:
         return "F", "Don't give up! Try again with more practice!"
     
+def get_valid_marks():
+    while True:
+        try:
+            marks = int(input("Enter marks (0-100): "))
+            if marks >= 0 and marks <= 100:
+                return marks
+            else:
+                print("Invalid marks! Please enter between 0 and 100.")
+        except:
+            print("Invalid input! Please enter numbers only.")
+
+
+def main():
+    name = input("Enter student name: ")
+    marks = get_valid_marks()
+    
+    grade, message = get_grade_and_message(marks)
+    
+    print("\nRESULT FOR", name.upper())
+    print("Marks:", marks, "/100")
+    print("Grade:", grade)
+    print("Message:", message)
+
+
+main()
